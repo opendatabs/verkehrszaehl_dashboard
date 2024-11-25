09d99f1e35da7c5396bf2667869ea249c3c428ad
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-11-25 16:57:03 +0100

    Vertical alignment of h3 and filters

--- src/common_components.js
+++ src/common_components.js
@@ -6,7 +6,7 @@ export function getFilterComponent() {
             <div id="filter-buttons">
                 <!-- Verkehrsmittel -->
                 <div class="filter-group">
-                    <h3>Verkehrsmittel</h3>
+                    <h3>Verkehrsmittel:</h3>
                     <input type="radio" id="filter-velo" name="filter" value="Velo">
                     <label for="filter-velo">
                         <img src="../../img/bicycle.png" alt="Velo" class="filter-icon"> Velo
@@ -22,7 +22,7 @@ export function getFilterComponent() {
                 </div>
                 <!-- Zählstelle -->
                 <div class="filter-group">
-                    <h3>Zählstelle</h3>
+                    <h3>Zählstelle:</h3>
                     <div class="custom-select">
                         <select id="counting-station-dropdown"></select>
                     </div>
@@ -40,7 +40,7 @@ export function getDayRangeButtonsComponent() {
             <div id="day-range-buttons">
                 <!-- Wochentage -->
                 <div class="filter-group">
-                    <h3>Wochentage</h3>
+                    <h3>Wochentage:</h3>
                     <input type="checkbox" id="mo-fr" value="Mo-Fr" checked>
                     <label for="mo-fr">Mo-Fr</label>
                     <input type="checkbox" id="sa-so" value="Sa-So" checked>
@@ -48,7 +48,7 @@ export function getDayRangeButtonsComponent() {
                 </div>
                 <!-- Zeitraum -->
                 <div class="filter-group">
-                    <h3>Zeitraum</h3>
+                    <h3>Zeitraum:</h3>
                     <label for="start-date">Von:</label>
                     <input type="date" id="start-date" name="start-date">
                     <label for="end-date">Bis:</label>
@@ -56,7 +56,7 @@ export function getDayRangeButtonsComponent() {
                 </div>
                 <!-- Zeiteinheit -->
                 <div class="filter-group">
-                    <h3>Zeiteinheit</h3>
+                    <h3>Zeiteinheit:</h3>
                     <input type="radio" id="zeitraum-1-tag" name="zeitraum" value="1 Tag">
                     <label for="zeitraum-1-tag">1 Tag</label>
                     <input type="radio" id="zeitraum-1-woche" name="zeitraum" value="1 Woche">
