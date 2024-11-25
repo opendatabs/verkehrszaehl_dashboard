54143b2d4c9ccce8f9828757d83739522640ee0d
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-11-25 16:27:35 +0100

    Add filters

--- src/common_components.js
+++ src/common_components.js
@@ -38,10 +38,36 @@ export function getDayRangeButtonsComponent() {
         type: 'HTML',
         html: `
             <div id="day-range-buttons">
-                <input type="checkbox" id="mo-fr" value="Mo-Fr" checked>
-                <label for="mo-fr">Mo-Fr</label>
-                <input type="checkbox" id="sa-so" value="Sa-So" checked>
-                <label for="sa-so">Sa+So</label>
+                <!-- Wochentage -->
+                <div class="filter-group">
+                    <h3>Wochentage</h3>
+                    <input type="checkbox" id="mo-fr" value="Mo-Fr" checked>
+                    <label for="mo-fr">Mo-Fr</label>
+                    <input type="checkbox" id="sa-so" value="Sa-So" checked>
+                    <label for="sa-so">Sa+So</label>
+                </div>
+                <!-- Zeitraum -->
+                <div class="filter-group">
+                    <h3>Zeitraum</h3>
+                    <label for="start-date">Von:</label>
+                    <input type="date" id="start-date" name="start-date">
+                    <label for="end-date">Bis:</label>
+                    <input type="date" id="end-date" name="end-date">
+                </div>
+                <!-- Zeiteinheit -->
+                <div class="filter-group">
+                    <h3>Zeiteinheit</h3>
+                    <input type="radio" id="zeitraum-1-tag" name="zeitraum" value="1 Tag">
+                    <label for="zeitraum-1-tag">1 Tag</label>
+                    <input type="radio" id="zeitraum-1-woche" name="zeitraum" value="1 Woche">
+                    <label for="zeitraum-1-woche">1 Woche</label>
+                    <input type="radio" id="zeitraum-1-monat" name="zeitraum" value="1 Monat">
+                    <label for="zeitraum-1-monat">1 Monat</label>
+                    <input type="radio" id="zeitraum-1-jahr" name="zeitraum" value="1 Jahr">
+                    <label for="zeitraum-1-jahr">1 Jahr</label>
+                    <input type="radio" id="zeitraum-alles" name="zeitraum" value="Alles" checked>
+                    <label for="zeitraum-alles">Alles</label>
+                </div>
             </div>
         `
     };
